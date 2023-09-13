ee998874414b96b41f00a9952e97eccecac48c2e
cherneysp <aaron.cherney@synesis-partners.com>
2023-09-13 14:32:28 -0500

    "BigDecimal(double)" should not be used

--- C2CRIBuildDir/projects/C2C-RI/src/RICenterServices/src/org/fhwa/c2cri/messagemanager/XmlGeneratorUtility.java
+++ C2CRIBuildDir/projects/C2C-RI/src/RICenterServices/src/org/fhwa/c2cri/messagemanager/XmlGeneratorUtility.java
@@ -634,7 +634,7 @@ public class XmlGeneratorUtility
                 increment = new BigDecimal(sb.toString());
             }
             else
-                increment = new BigDecimal(1.0);
+                increment = BigDecimal.valueOf(1.0);
         }
 
         if (minOk && maxOk)
@@ -776,7 +776,7 @@ public class XmlGeneratorUtility
             if (gdurb.getSecond() <= minExclusive.getSecond())
                 gdurb.setSecond(minExclusive.getSecond()+1);
             if (gdurb.getFraction().compareTo(minExclusive.getFraction()) <= 0)
-                gdurb.setFraction(minExclusive.getFraction().add(new BigDecimal(0.001)));
+                gdurb.setFraction(minExclusive.getFraction().add(BigDecimal.valueOf(0.001)));
         }
 
         if (maxExclusive != null)
