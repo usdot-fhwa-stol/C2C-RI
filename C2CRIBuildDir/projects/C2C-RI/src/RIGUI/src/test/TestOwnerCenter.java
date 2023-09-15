097cda8a8dfb3ecf5c8c98948d00f480fc7587e6
cherneysp <aaron.cherney@synesis-partners.com>
2023-09-15 09:14:12 -0500

    Null pointers should not be dereferenced

--- C2CRIBuildDir/projects/C2C-RI/src/RIGUI/src/test/TestOwnerCenter.java
+++ C2CRIBuildDir/projects/C2C-RI/src/RIGUI/src/test/TestOwnerCenter.java
@@ -79,126 +79,128 @@ public class TestOwnerCenter {
 //        ApplicationLayerStandard als = new NTCIP2306ApplicationLayerStandard();
 //        InformationLayerStandard tmdd = new TMDDv303InformationLayerStandard();
 //        als.initializeStandard(wsdlURL, null, "OC", "TMDD", "FakeTestCase", null, null, null);
-        tmdd.initializeStandard("FakeName", "OC", als);
-        //       RIWSDL theWSDL = new RIWSDL(tc.getWsdlFile());
-        //       CenterConfigurationController ccc2 = new CenterConfigurationController(null, theWSDL, Center.RICENTERMODE.OC);
+		if (tmdd != null)
+		{
+			tmdd.initializeStandard("FakeName", "OC", als);
+			//       RIWSDL theWSDL = new RIWSDL(tc.getWsdlFile());
+			//       CenterConfigurationController ccc2 = new CenterConfigurationController(null, theWSDL, Center.RICENTERMODE.OC);
 
-//        OperationIdentifier pubOpId = null;
-//            HashMap<OperationIdentifier, OperationIdentifier> subPubMap = new HashMap();
-//        if (tc.getOperationType().equals(tc.getOperationType().HTTPSOAPSUB)) {
-//            OperationIdentifier subOpId = new OperationIdentifier(tc.getServiceName(), tc.getPortName(), tc.getOperationName(),
-//                    OperationIdentifier.SOURCETYPE.HANDLER);
-//            pubOpId = new OperationIdentifier(tc.getRelatedServiceName(), tc.getRelatedPortName(), tc.getRelatedOperationName(),
-//                    OperationIdentifier.SOURCETYPE.LISTENER);
-////
-//            subPubMap.put(subOpId, pubOpId);
-////            String subPubMapping = tc.getServiceName()+","+tc.getPortName()+","+tc.getOperationName()+":"+tc.getRelatedServiceName()+","+tc.getRelatedPortName()+","+tc.getRelatedOperationName()+";fakeService,fakePort,fakeOperation:fakeServie,fakePort,fakeOperation;badService,badOperation,badPort;";
-////            ccc2.setSubPubMapping(subPubStringMapConverter(subPubMapping, Center.RICENTERMODE.OC.name()));
-//        }
+	//        OperationIdentifier pubOpId = null;
+	//            HashMap<OperationIdentifier, OperationIdentifier> subPubMap = new HashMap();
+	//        if (tc.getOperationType().equals(tc.getOperationType().HTTPSOAPSUB)) {
+	//            OperationIdentifier subOpId = new OperationIdentifier(tc.getServiceName(), tc.getPortName(), tc.getOperationName(),
+	//                    OperationIdentifier.SOURCETYPE.HANDLER);
+	//            pubOpId = new OperationIdentifier(tc.getRelatedServiceName(), tc.getRelatedPortName(), tc.getRelatedOperationName(),
+	//                    OperationIdentifier.SOURCETYPE.LISTENER);
+	////
+	//            subPubMap.put(subOpId, pubOpId);
+	////            String subPubMapping = tc.getServiceName()+","+tc.getPortName()+","+tc.getOperationName()+":"+tc.getRelatedServiceName()+","+tc.getRelatedPortName()+","+tc.getRelatedOperationName()+";fakeService,fakePort,fakeOperation:fakeServie,fakePort,fakeOperation;badService,badOperation,badPort;";
+	////            ccc2.setSubPubMapping(subPubStringMapConverter(subPubMapping, Center.RICENTERMODE.OC.name()));
+	//        }
 
-//        ccc2.initialzeServices();
-//        if (tc.getRelatedRequestMessage() != null) {
-//            ArrayList<NTCIP2306lMessage> msgList = new ArrayList();
-//            msgList.add(new NTCIP2306Message(tc.getRelatedRequestMessage()));
-//            pubOpId = new OperationIdentifier(tc.getRelatedServiceName(), tc.getRelatedPortName(), tc.getRelatedOperationName(),
-//                    OperationIdentifier.SOURCETYPE.HANDLER);
-//            ccc2.setOCPublicationMessages(pubOpId, msgList);
-//        }
-//        NTCIP2306Controller ntcip = new NTCIP2306Controller(theWSDL, ccc2);
+	//        ccc2.initialzeServices();
+	//        if (tc.getRelatedRequestMessage() != null) {
+	//            ArrayList<NTCIP2306lMessage> msgList = new ArrayList();
+	//            msgList.add(new NTCIP2306Message(tc.getRelatedRequestMessage()));
+	//            pubOpId = new OperationIdentifier(tc.getRelatedServiceName(), tc.getRelatedPortName(), tc.getRelatedOperationName(),
+	//                    OperationIdentifier.SOURCETYPE.HANDLER);
+	//            ccc2.setOCPublicationMessages(pubOpId, msgList);
+	//        }
+	//        NTCIP2306Controller ntcip = new NTCIP2306Controller(theWSDL, ccc2);
 
-//        InformationLayerAdapter ila = new NTCIP2306InformationLayerAdapter(ntcip);
+	//        InformationLayerAdapter ila = new NTCIP2306InformationLayerAdapter(ntcip);
 
-        InformationLayerController ilc = tmdd.getInformationLayerController();
-        ilc.setDisableAppLayerEncoding(tc.isSkipEncoding());
-//        final HashMap<OperationIdentifier,OperationIdentifier> finalHashMap = subPubMap;
-//        final String subOperation = tc.getOperationName();
-//        final String pubOperation = tc.getRelatedOperationName();
-//        als.setSubPubMapper(new SubPubMapper(){
-//
-//            @Override
-//            public String getRelatedPublicationName(String subscriptionOperationName, Message requestMessage) throws SubPubMappingException {
-//                return (pubOperation);
-//            }
-//
-//            @Override
-//            public String getRelatedSubscriptionName(String publicationOperationName) throws SubPubMappingException {
-//                return subOperation;
-//            }
-//            
-//        });
-        try {
-            if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.FTPGET)
-                    || tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPGET)) {
-                Message responseMsg = MessageManager.getInstance().createMessage("Response");
-                responseMsg.setMessageBody(tc.getResponseMessage());
-                results = ilc.performGetOC(tc.getOperationName(),
-                        responseMsg);
+			InformationLayerController ilc = tmdd.getInformationLayerController();
+			ilc.setDisableAppLayerEncoding(tc.isSkipEncoding());
+	//        final HashMap<OperationIdentifier,OperationIdentifier> finalHashMap = subPubMap;
+	//        final String subOperation = tc.getOperationName();
+	//        final String pubOperation = tc.getRelatedOperationName();
+	//        als.setSubPubMapper(new SubPubMapper(){
+	//
+	//            @Override
+	//            public String getRelatedPublicationName(String subscriptionOperationName, Message requestMessage) throws SubPubMappingException {
+	//                return (pubOperation);
+	//            }
+	//
+	//            @Override
+	//            public String getRelatedSubscriptionName(String publicationOperationName) throws SubPubMappingException {
+	//                return subOperation;
+	//            }
+	//            
+	//        });
+			try {
+				if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.FTPGET)
+						|| tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPGET)) {
+					Message responseMsg = MessageManager.getInstance().createMessage("Response");
+					responseMsg.setMessageBody(tc.getResponseMessage());
+					results = ilc.performGetOC(tc.getOperationName(),
+							responseMsg);
 
-            } else if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPPOST)
-                    || tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPRR)
-                    || (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPRR))) {
-                results = ilc.performRequestResponseOCReceive(tc.getOperationName());
-                System.out.println("\n\nREQUEST RESULTS\n");
-                System.out.println(results.toString());
-                Message responseMsg = MessageManager.getInstance().createMessage("Response");
-                responseMsg.setMessageBody(tc.getResponseMessage());
-                results = ilc.performRequestResponseOCResponse(tc.getOperationName(),
-                        responseMsg, tc.isMessageErrorExpected());
+				} else if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPPOST)
+						|| tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPRR)
+						|| (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPRR))) {
+					results = ilc.performRequestResponseOCReceive(tc.getOperationName());
+					System.out.println("\n\nREQUEST RESULTS\n");
+					System.out.println(results.toString());
+					Message responseMsg = MessageManager.getInstance().createMessage("Response");
+					responseMsg.setMessageBody(tc.getResponseMessage());
+					results = ilc.performRequestResponseOCResponse(tc.getOperationName(),
+							responseMsg, tc.isMessageErrorExpected());
 
-            } else if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPSUB)) {
-                results = ilc.performSubscriptionOCReceive(tc.getOperationName());
-                System.out.println("\n\nSOAP SUB REQUEST RESULTS\n");
-                System.out.println("SubscriptionActive: " + results.isSubscriptionActive() + "  PubComplete: " + results.isPublicationComplete() + "  Pub Count" + results.getPublicationCount());
+				} else if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPSUB)) {
+					results = ilc.performSubscriptionOCReceive(tc.getOperationName());
+					System.out.println("\n\nSOAP SUB REQUEST RESULTS\n");
+					System.out.println("SubscriptionActive: " + results.isSubscriptionActive() + "  PubComplete: " + results.isPublicationComplete() + "  Pub Count" + results.getPublicationCount());
 
-                Message responseMsg = MessageManager.getInstance().createMessage("Response");
-                responseMsg.setMessageBody(tc.getResponseMessage());
-                results = ilc.performSubscriptionOCResponse(tc.getOperationName(),
-                        responseMsg, tc.isMessageErrorExpected());
-                System.out.println("\n\nSOAP SUB RESPONSE RESULTS\n");
-                System.out.println("SubscriptionActive: " + results.isSubscriptionActive() + "  PubComplete: " + results.isPublicationComplete() + "  Pub Count" + results.getPublicationCount());
+					Message responseMsg = MessageManager.getInstance().createMessage("Response");
+					responseMsg.setMessageBody(tc.getResponseMessage());
+					results = ilc.performSubscriptionOCResponse(tc.getOperationName(),
+							responseMsg, tc.isMessageErrorExpected());
+					System.out.println("\n\nSOAP SUB RESPONSE RESULTS\n");
+					System.out.println("SubscriptionActive: " + results.isSubscriptionActive() + "  PubComplete: " + results.isPublicationComplete() + "  Pub Count" + results.getPublicationCount());
 
-                while (results.isSubscriptionActive() && (!results.isPublicationComplete()) && results.getPublicationCount() <= tc.getNumPublicationsExpected()) {
-                    System.out.println("Current Subscription Status = " + results.isSubscriptionActive() + " Pub Count: " + results.getPublicationCount());
-                    Message requestMsg = MessageManager.getInstance().createMessage("Response");
-                    requestMsg.setMessageBody(tc.getRelatedRequestMessage());
-                    results = ilc.performPublicationOC(tc.getRelatedOperationName(),
-                            requestMsg);
-//            Thread.sleep(15000);
-                }
+					while (results.isSubscriptionActive() && (!results.isPublicationComplete()) && results.getPublicationCount() <= tc.getNumPublicationsExpected()) {
+						System.out.println("Current Subscription Status = " + results.isSubscriptionActive() + " Pub Count: " + results.getPublicationCount());
+						Message requestMsg = MessageManager.getInstance().createMessage("Response");
+						requestMsg.setMessageBody(tc.getRelatedRequestMessage());
+						results = ilc.performPublicationOC(tc.getRelatedOperationName(),
+								requestMsg);
+	//            Thread.sleep(15000);
+					}
 
-            } else if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPPUB)) {
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+				} else if (tc.getOperationType().equals(TestCaseSpec.OPERATIONTYPE.HTTPSOAPPUB)) {
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 
-        ilc.shutdown();
-//        ccc2.stopServices();
-//        System.out.println("\n\n\n");
-//        System.out.println(results.toString());
-//        try {
-//            testMessage(results, "Request");
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-        System.gc();
+			ilc.shutdown();
+	//        ccc2.stopServices();
+	//        System.out.println("\n\n\n");
+	//        System.out.println(results.toString());
+	//        try {
+	//            testMessage(results, "Request");
+	//        } catch (Exception ex) {
+	//            ex.printStackTrace();
+	//        }
+			System.gc();
 
 
-//        RIWSDL theWSDL = new RIWSDL("file:/c:/c2cri/testfiles/release3.wsdl");
-//        String serviceName = "tmddOCSoapHttpService";
-////        String portName = "tmddOCFtpGetServicePort";
-////        String operationName = "OP_RIXMLFtpDMSInventory";
-//        String portName = "tmddOCHttpGetServicePort";
-//        String operationName = "OP_RIXMLHttpGetDMSInventory";
-//        CenterConfigurationController ccc2 = new CenterConfigurationController(null, theWSDL, Center.RICENTERMODE.OC);
-//        ccc2.initialzeServices();
-//    
-//        NTCIP2306Controller ntcip = new NTCIP2306Controller();
-////        NTCIP2306ControllerResults results = ntcip.performFTPGetOC(serviceName, portName, operationName, "<Message>This is the Response Message</Message>",ccc2);
-////        NTCIP2306ControllerResults results = ntcip.performFTPGetOC(serviceName, portName, operationName, "<deviceInformationRequestMsg>This is the Response Message</deviceInformationRequestMsg>",ccc2);
-////        NTCIP2306ControllerResults results = ntcip.performFTPGetOC(serviceName, portName, operationName, "<deviceInformationRequestMsg xmlns=\"http://www.tmdd.org/3/messages\">This is the Response Message</deviceInformationRequestMsg>",ccc2,false);
-//        NTCIP2306ControllerResults results = ntcip.performHTTPGetOC(serviceName, portName, operationName, "<deviceInformationRequestMsg xmlns=\"http://www.tmdd.org/3/messages\">This is the Response Message</deviceInformationRequestMsg>",ccc2,false);
-
+	//        RIWSDL theWSDL = new RIWSDL("file:/c:/c2cri/testfiles/release3.wsdl");
+	//        String serviceName = "tmddOCSoapHttpService";
+	////        String portName = "tmddOCFtpGetServicePort";
+	////        String operationName = "OP_RIXMLFtpDMSInventory";
+	//        String portName = "tmddOCHttpGetServicePort";
+	//        String operationName = "OP_RIXMLHttpGetDMSInventory";
+	//        CenterConfigurationController ccc2 = new CenterConfigurationController(null, theWSDL, Center.RICENTERMODE.OC);
+	//        ccc2.initialzeServices();
+	//    
+	//        NTCIP2306Controller ntcip = new NTCIP2306Controller();
+	////        NTCIP2306ControllerResults results = ntcip.performFTPGetOC(serviceName, portName, operationName, "<Message>This is the Response Message</Message>",ccc2);
+	////        NTCIP2306ControllerResults results = ntcip.performFTPGetOC(serviceName, portName, operationName, "<deviceInformationRequestMsg>This is the Response Message</deviceInformationRequestMsg>",ccc2);
+	////        NTCIP2306ControllerResults results = ntcip.performFTPGetOC(serviceName, portName, operationName, "<deviceInformationRequestMsg xmlns=\"http://www.tmdd.org/3/messages\">This is the Response Message</deviceInformationRequestMsg>",ccc2,false);
+	//        NTCIP2306ControllerResults results = ntcip.performHTTPGetOC(serviceName, portName, operationName, "<deviceInformationRequestMsg xmlns=\"http://www.tmdd.org/3/messages\">This is the Response Message</deviceInformationRequestMsg>",ccc2,false);
+		}
 
     }
 
