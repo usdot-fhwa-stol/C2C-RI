5de2589fe1d5717f950fad22051640044fe9f13c
cherneysp <aaron.cherney@synesis-partners.com>
2023-09-18 08:28:15 -0500

    Null pointers should not be dereferenced

--- C2CRIBuildDir/projects/C2C-RI/src/TMDDV3Verification/src/tmddv3verificationold/utilities/inouts/SampleXmlUtilDoctored.java
+++ C2CRIBuildDir/projects/C2C-RI/src/TMDDV3Verification/src/tmddv3verificationold/utilities/inouts/SampleXmlUtilDoctored.java
@@ -386,15 +386,19 @@ public class SampleXmlUtilDoctored {
     private String getMinInclusive(SchemaType sType) {
         XmlDecimal xmlD;
         xmlD = (XmlDecimal) sType.getFacet(SchemaType.FACET_MIN_INCLUSIVE);
-        BigDecimal min = xmlD != null ? xmlD.getBigDecimalValue() : null;
-        return min.toString();
+		if (xmlD != null)
+			return xmlD.getBigDecimalValue().toString();
+		
+		return null;
     }
 
     private String getMaxInclusive(SchemaType sType) {
         XmlDecimal xmlD;
         xmlD = (XmlDecimal) sType.getFacet(SchemaType.FACET_MAX_INCLUSIVE);
-        BigDecimal max = xmlD != null ? xmlD.getBigDecimalValue() : null;
-        return max.toString();
+		if (xmlD != null)
+			return xmlD.getBigDecimalValue().toString();
+		
+		return null;
     }
 
     private String sampleDataForSimpleType(SchemaType sType) {
