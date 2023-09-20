3a78fc88face2532611cbcf1294ed2a6d061725c
cherneysp <aaron.cherney@synesis-partners.com>
2023-09-20 13:53:21 -0500

    "InterruptedException" should not be ignored

--- C2CRIBuildDir/projects/C2C-RI/src/RIGUI/src/test/progresstest/BasicGUIActionWrapper.java
+++ C2CRIBuildDir/projects/C2C-RI/src/RIGUI/src/test/progresstest/BasicGUIActionWrapper.java
@@ -57,6 +57,8 @@ public class BasicGUIActionWrapper extends SwingWorker<Boolean, Void> {
         try {
             wrapUp(this.get());
         } catch (Exception ex) {
+			if (ex instanceof InterruptedException)
+				Thread.currentThread().interrupt();
             ex.printStackTrace();
             javax.swing.JOptionPane.showMessageDialog(parentFrame, "An error was encountered trying to complete the " + this.theActionName + " action.\n"+ex.getMessage());
         }
